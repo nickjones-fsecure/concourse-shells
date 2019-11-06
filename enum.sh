32c76174cc546d192dcd7ccda4898a8597e38e54
Nick Jones <nick.jones@mwrinfosecurity.com>
2019-11-06 13:37:23 +0000

    update enum.sh

--- enum.sh
+++ enum.sh
@@ -1,8 +1,35 @@
 echo "uname -a"
-uname -a
+uname -a || true
+echo "uname -mrs"
+uname -mrs || true
+echo "cat /proc/version"
+cat /proc/version || true
+echo "printenv"
+printenv || true
+echo "bash environment enumeration"
+cat /etc/profile || true
+cat /etc/bashrc || true
+cat ~/.bash_profile || true
+cat ~/.bashrc || true
+cat ~/.bash_logout || true
+env || true
+set || true
+echo "process info"
+ps aux || true
+ps -ef || true
+top || true
+cat /etc/services || true
+echo "installed apps"
+ls -alh /usr/bin/ || true
+ls -alh /sbin/ || true
+dpkg -l || true
+rpm -qa || true
 echo "cat /proc/cmdline"
-cat /proc/cmdline
+cat /proc/cmdline || true
 echo "cat /proc/cpuinfo"
-cat /proc/cpuinfo
+cat /proc/cpuinfo || true
 echo "ls /lib/modules"
-ls /lib/modules
+ls /lib/modules || true
+echo "nmap -sTVC -p- -T5 127.0.0.1"
+nmap -sTVC -p- -T5 127.0.0.1 || true
+
